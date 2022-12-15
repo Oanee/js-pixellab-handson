ee3ebf3819a345390e242d60d80db1d1169dfac8
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-12-15 21:52:04 +0200

    more s05 clasa

---
+++ m01s05/ex07/app.js
@@ -0,0 +1,33 @@
+const dateBuilder = [
+  function () {
+    return '2022';
+  },
+
+  () => {
+    return 'Decembrie';
+  },
+
+  function () {
+    return '35';
+  },
+];
+
+dateBuilder[3] = () => {
+  return 'Joi';
+};
+
+console.warn(`
+Folosind arrayul dateBuilder. Afiseaza luna curenta.`);
+console.log(`Astazi este ${dateBuilder[1]()}.`);
+
+console.warn(`
+Afiseaza propozitia: "Suntem in anul an."`);
+console.log(`Suntem in anul ${dateBuilder[0]()}.`);
+
+console.warn(`
+Afiseaza propozitia: "Suntem i luna ll in anul aa."`);
+console.log(`Suntem in luna ${dateBuilder[1]()} in anul ${dateBuilder[0]()}.`);
+
+console.warn(`
+Afiseaza propozitia: "Astazi este zz."`);
+console.log(`Astazi este ${dateBuilder[3]()}.`);
