3faca6e145189ed5d5abbcc8cb8759f7caf000b7
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2023-01-17 21:55:01 +0200

    s06 clasa

--- m01s06/ex04/app.js
+++ m01s06/ex04/app.js
@@ -5,20 +5,30 @@
     stage.addEventListener('mouseover', () => {
       const message = 'Mousul este pe scena';
       console.log(message);
-      showMessage(message);
+      // showMessage(message);
+      createParagraph(message, 'message');
     });
 
     stage.addEventListener('mouseout', () => {
       const message = 'Mouseul nu este pe scena';
       console.log(message);
-      showMessage(message);
+      // showMessage(message);
+      createParagraph(message, 'message');
     });
 
     // function functions are hoisted
-    function showMessage(message) {
+    // function showMessage(message) {
+    //   const paragraphElement = document.createElement('p');
+    //   paragraphElement.innerText = message;
+    //   paragraphElement.classList.add('message');
+
+    //   document.body.append(paragraphElement);
+    // }
+
+    function createParagraph(message, cssClass = '') {
       const paragraphElement = document.createElement('p');
       paragraphElement.innerText = message;
-      paragraphElement.classList.add('message');
+      paragraphElement.classList.add(cssClass);
 
       document.body.append(paragraphElement);
     }
